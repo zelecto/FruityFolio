0e5759e869284fcfd2b90385f9cfd68fd7eca011
zelecto <ca346364@gmail.com>
2024-06-01 15:28:14 -0500

    Implementacion de JWT

--- src/assets/Components/Base/BdCliente.jsx
+++ src/assets/Components/Base/BdCliente.jsx
@@ -1,24 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const url = "https://localhost:7208";
+
+// Función para obtener el token almacenado
+function getToken() {
+    return localStorage.getItem('token'); // Asegúrate de almacenar el token en el localStorage cuando inicias sesión
+}
+
+// Configurar el interceptor para incluir el token en cada solicitud
+axios.interceptors.request.use(config => {
+    const token = getToken();
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
 const Clientes = () => {
     const [clientes, setClientes] = useState([]);
 
     useEffect(() => {
         const fetchClientes = async () => {
             try {
-                
-                const response = await axios.get(`${url}+/api/Clientes`);
+                const response = await axios.get(`${url}/api/Clientes`);
                 setClientes(response.data);
             } catch (error) {
                 console.error('Error fetching clientes:', error);
             }
-            
         };
-        console.log(clientes);
+
         fetchClientes();
     }, []);
-    
 
     return (
         <div className="container mx-auto">
